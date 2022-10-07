103bce4e7f744ddaaa5c7c7a293140697002d41b
Soneech <sonya.patrina@yandex.ru>
2022-10-07 15:53:59 +0300

    practice 7 task 2

--- src/practice7/task3/DrunkardDeque.java
+++ src/practice7/task3/DrunkardDeque.java
@@ -1,22 +1,22 @@
-package practice7.task1;
+package practice7.task3;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
-import java.util.Stack;
 
-public class DrunkardStack {
-    public static void game(Stack<Integer> player1, Stack<Integer> player2) {
+public class DrunkardDeque {
+    public static void game(Deque<Integer> player1, Deque<Integer> player2) {
         int steps = 0, card1, card2;
-
-        while(!player1.isEmpty() && !player2.isEmpty() && steps < 106) {
+        while (steps < 106 && !player1.isEmpty() && !player2.isEmpty()) {
             card1 = player1.pop();
             card2 = player2.pop();
             if ((card1 == 0 && card2 == 9) || (card1 > card2 && card2 != 0 && card1 != 9)) {
-                player1.add(0, card1);
-                player1.add(0, card2);
+                player1.addLast(card1);
+                player1.addLast(card2);
             }
             else if ((card1 == 9 && card2 == 0) || (card1 < card2)) {
-                player2.add(0, card1);
-                player2.add(0, card2);
+                player2.addLast(card1);
+                player2.addLast(card2);
             }
             steps++;
 //            System.out.println("step: " + steps);
@@ -36,16 +36,16 @@ public class DrunkardStack {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] numbers1 = scanner.nextLine().split(" ");
-        String[] numbers2 = scanner.nextLine().split(" ");
+        Deque<Integer> player1 = new ArrayDeque<>();
+        Deque<Integer> player2 = new ArrayDeque<>();
 
-        Stack<Integer> player1 = new Stack<>();
-        Stack<Integer> player2 = new Stack<>();
-
-        for (int i = numbers1.length - 1; i >= 0; i--) {
-            player1.push(Integer.valueOf(numbers1[i]));
-            player2.push(Integer.valueOf(numbers2[i]));
+        for (int i = 0; i < 5; i++) {
+            player1.addLast(scanner.nextInt());
         }
+        for (int i = 0; i < 5; i++) {
+            player2.addLast(scanner.nextInt());
+        }
+
         game(player1, player2);
     }
 }
