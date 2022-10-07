a69d2ce3e9662a693bb96f2b935149d0867bb597
Soneech <sonya.patrina@yandex.ru>
2022-10-07 14:30:19 +0300

    output in practice 7 task 1

--- src/practice7/task1/DrunkardStack.java
+++ src/practice7/task1/DrunkardStack.java
@@ -1,6 +1,5 @@
 package practice7.task1;
 
-import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -8,7 +7,7 @@ public class DrunkardStack {
     public static void game(Stack<Integer> player1, Stack<Integer> player2) {
         int steps = 0;
 
-        while(!player1.isEmpty() && !player2.isEmpty() && steps <= 106) {
+        while(!player1.isEmpty() && !player2.isEmpty() && steps < 106) {
             int card1 = player1.peek();
             int card2 = player2.peek();
             if ((card1 == 0 && card2 == 9) || (card1 > card2 && card2 != 0 && card1 != 9)) {
@@ -20,10 +19,19 @@ public class DrunkardStack {
                 player2.add(0, player2.pop());
             }
             steps++;
-            System.out.println("step: " + steps);
-            System.out.println(player1);
-            System.out.println(player2);
+//            System.out.println("step: " + steps);
+//            System.out.println(player1);
+//            System.out.println(player2);
         }
+
+        if (steps != 106) {
+            if (player1.isEmpty())
+                System.out.println("second " + steps);
+            else
+                System.out.println("first " + steps);
+        }
+        else
+            System.out.println("botva");
     }
 
     public static void main(String[] args) {
