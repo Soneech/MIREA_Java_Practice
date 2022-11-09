0227fde47ee93bbc03c009b69abd0e1bbf85b9c4
Soneech <sonya.patrina@yandex.ru>
2022-11-09 21:11:28 +0300

    practice 17-18, task 3

--- src/practice17_18/task1/FirstTask.java
+++ src/practice17_18/task1/FirstTask.java
@@ -1,16 +1,17 @@
-package practice17_18.task2;
+package practice17_18.task1;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class FirstTask {
     public static void main(String[] args) {
-        String regex = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$";
+        String regex = "^abcdefghijklmnopqrstuv18340$";
 
-        String test1 = "aE:dC:cA:56:76:54";
-        String test2 = "11:23:45:67:89:Az";
+        String test1 = "abcdefghijklmnopqrstuv18340";
+        String test2 = "abcdefghijklmnoasdfasdpqrstuv18340";
 
         Pattern pattern = Pattern.compile(regex);
+
         Matcher matcher1 = pattern.matcher(test1);
         Matcher matcher2 = pattern.matcher(test2);
 
--- src/practice17_18/task2/SecondTask.java
+++ src/practice17_18/task2/SecondTask.java
@@ -1,17 +1,16 @@
-package practice17_18.task1;
+package practice17_18.task2;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class SecondTask {
     public static void main(String[] args) {
-        String regex = "^abcdefghijklmnopqrstuv18340$";
+        String regex = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$";
 
-        String test1 = "abcdefghijklmnopqrstuv18340";
-        String test2 = "abcdefghijklmnoasdfasdpqrstuv18340";
+        String test1 = "aE:dC:cA:56:76:54";
+        String test2 = "11:23:45:67:89:Az";
 
         Pattern pattern = Pattern.compile(regex);
-
         Matcher matcher1 = pattern.matcher(test1);
         Matcher matcher2 = pattern.matcher(test2);
 
